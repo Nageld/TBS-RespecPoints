bd2770f8ffde84b595ca87e976355443fc239416
nageld <nageld@bxscience.edu>
2024-08-30 00:40:10 -0400

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Wizards.Perks;
+
+namespace RespecPoints;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PerkManager))]
+    public static class RespecPointsPatch
+    {
+        [HarmonyPatch("respecPoints", MethodType.Getter)]
+        [HarmonyPostfix]
+        public static void GetRespecPointsPostfix(ref int __result)
+        {
+            __result = 10; 
+        }
+    }
+}
